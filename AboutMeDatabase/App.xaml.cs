b10ff76483d096bff923398bb204c029ebf32315
Tyler Bach <Tbach2@my.wctc.edu>
2022-05-15 15:22:31 -0700

    Add project files.

---
+++ AboutMeDatabase/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using AboutMeDatabase.Data;
+using AboutMeDatabase.Models;
+using Xamarin.Forms;
+
+
+namespace AboutMeDatabase
+{
+    public partial class App : Application
+    {
+        static PersonalItemDatabase database;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        public static PersonalItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new PersonalItemDatabase(); 
+                    prefillDatabase();
+
+                }
+                return database;
+            }
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        static void prefillDatabase()
+        {
+            database.ClearAllAsync();
+            List<PersonalItem> items = new List<PersonalItem>();
+                items.Add(new PersonalItem() { Title = "Name", Detail = "Tyler Bach " });
+                items.Add(new PersonalItem() { Title = "Job", Detail = "Student " });
+                database.InsertList(items);
+
+        }
+    }
+}
